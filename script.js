ae1689357d7400865cf474e31987e7089f5a55f7
{NilK-Albert} <{khanhduyxtkd@gmail.com}>
2024-05-15 18:45:58 +0700

    #3 add right section

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// script
+const allVideos = document.querySelectorAll('video')
+const sidebar = document.querySelector('.left-section')
+const sidebarItems = document.querySelectorAll('.sidebar .item')
+const catItems = document.querySelectorAll('.categories a')
+
+// Play/Pause video on mouse enter/leave
+allVideos.forEach(video => {
+  video.addEventListener('mouseover', () => {
+    video.play()
+  })
+  video.addEventListener('mouseleave', () => {
+    video.pause()
+  })
+})
+
+// Handle sidebar item click
+sidebarItems.forEach(sideItem => {
+  sideItem.addEventListener('click', () => {
+    sidebarItems.forEach(item => {
+      item.classList.remove('active')
+    })
+    sideItem.classList.add('active')
+  })
+})
+
+// Handle category item click
+catItems.forEach(catItem => {
+  catItem.addEventListener('click', () => {
+    catItems.forEach(item => {
+      item.classList.remove('active')
+    })
+    catItem.classList.add('active')
+  })
+})
